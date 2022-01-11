7855f05fa267db43c203df0a47108c48d556dd7d
Lazar Jovanovic <lazar.jovanovic@nissatech.com>
2022-01-11 22:08:31 +0100

    Added functionality to work with https and created self signed certificates

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from datetime import datetime
 from fast_api_methods import app
 
 if __name__ == '__main__':
+    key_path = "certificates/example.key"
+    cert_path = "certificates/example.crt"
+
     print(f'Service started at {datetime.now()}')
     try:
-        uvicorn.run(app, host='localhost', port=7777)
+        uvicorn.run(app, host='localhost', port=7777, ssl_keyfile=key_path, ssl_certfile=cert_path)
     except Exception as e:
         print(f'Service stopped at {datetime.now()} for reason: {e}')
